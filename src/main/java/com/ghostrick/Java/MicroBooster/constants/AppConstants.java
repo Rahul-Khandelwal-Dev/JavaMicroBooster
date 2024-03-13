34928dc67ee33a50e97ee3fea7cd5235c21a0020
Rahul Khandelwal <163019225+Rahul-Khandelwal-Dev@users.noreply.github.com>
2024-03-13 23:46:14 +0800

    update in controller

--- src/main/java/com/ghostrick/Java/MicroBooster/constants/AppConstants.java
+++ src/main/java/com/ghostrick/Java/MicroBooster/constants/AppConstants.java
@@ -2,5 +2,8 @@ package com.ghostrick.Java.MicroBooster.constants;
 
 public interface AppConstants {
     String BASE_PATH = "/api";
-    String MICRO_API = "/micro";
+    String MICRO_POST_API = "/micropost";
+
+    String HTTP_STATUS_200 = "200";
+    String HTTP_STATUS_200_DESC = "Successful";
 }
--- src/main/java/com/ghostrick/Java/MicroBooster/controller/MicroController.java
+++ src/main/java/com/ghostrick/Java/MicroBooster/controller/MicroController.java
@@ -2,8 +2,11 @@ package com.ghostrick.Java.MicroBooster.controller;
 
 import com.ghostrick.Java.MicroBooster.constants.AppConstants;
 import com.ghostrick.Java.MicroBooster.service.MicroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,9 @@ public class MicroController {
     @Autowired
     MicroService microService;
 
-    @PostMapping(AppConstants.MICRO_API)
+    @PostMapping(AppConstants.MICRO_POST_API)
+    @Operation(summary = "Micro Post API", description = "Sample Post API")
+    @ApiResponse(responseCode = AppConstants.HTTP_STATUS_200, description = AppConstants.HTTP_STATUS_200_DESC, useReturnTypeSchema = true)
     public String hello() {
         return "Hello, world!";
     }
